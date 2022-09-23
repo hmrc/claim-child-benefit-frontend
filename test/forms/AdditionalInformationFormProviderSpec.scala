c1e4e9f1008edf64698ae85c771aa70abaa1cfac
Nick Wilson <7313396+TheNickWilson@users.noreply.github.com>
2022-09-23 13:10:43 +0100

    Skeleton of additional information page

---
+++ test/forms/AdditionalInformationFormProviderSpec.scala
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package forms
+
+import forms.behaviours.StringFieldBehaviours
+import org.scalacheck.Gen
+import play.api.data.FormError
+
+class AdditionalInformationFormProviderSpec extends StringFieldBehaviours {
+
+  val requiredKey = "additionalInformation.error.required"
+  val lengthKey = "additionalInformation.error.length"
+
+  val form = new AdditionalInformationFormProvider()()
+
+  ".value" - {
+
+    val fieldName = "value"
+
+    behave like fieldThatBindsValidData(
+      form,
+      fieldName,
+      stringsWithMaxLength(1000)
+    )
+
+    behave like mandatoryField(
+      form,
+      fieldName,
+      requiredError = FormError(fieldName, requiredKey)
+    )
+
+    behave like fieldWithMaxLength(form, fieldName, 1000, FormError(fieldName, lengthKey, Seq(1000)))
+  }
+}
