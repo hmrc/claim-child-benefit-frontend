735be9053a89c47f3afc33361e11944d81c74b60
Nick Wilson <7313396+TheNickWilson@users.noreply.github.com>
2022-10-27 08:56:46 +0100

    Add `Guardian Name` page

---
+++ app/forms/child/GuardianNameFormProvider.scala
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package forms.child
+
+import forms.mappings.Mappings
+import models.{AdultName, ChildName}
+import play.api.data.Form
+import play.api.data.Forms._
+
+import javax.inject.Inject
+
+class GuardianNameFormProvider @Inject() extends Mappings {
+
+  def apply(childName: ChildName): Form[AdultName] = Form(
+    mapping(
+      "firstName" -> text("guardianName.error.firstName.required", args = Seq(childName.firstName))
+        .verifying(maxLength(100, "guardianName.error.firstName.length", childName.firstName)),
+      "middleNames" -> optional(text("guardianName.error.middleNames.required", args = Seq(childName.firstName))
+        .verifying(maxLength(100, "guardianName.error.middleNames.length", childName.firstName))),
+      "lastName" -> text("guardianName.error.lastName.required", args = Seq(childName.firstName))
+        .verifying(maxLength(100, "guardianName.error.lastName.length", childName.firstName))
+    )(AdultName.apply)(AdultName.unapply)
+  )
+}
---
+++ test/forms/child/GuardianNameFormProviderSpec.scala
@@ -0,0 +1,102 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package forms.child
+
+import forms.behaviours.StringFieldBehaviours
+import models.ChildName
+import play.api.data.FormError
+
+class GuardianNameFormProviderSpec extends StringFieldBehaviours {
+
+  private val name = ChildName("first", None, "last")
+  private val form = new GuardianNameFormProvider()(name)
+
+  ".firstName" - {
+
+    val fieldName = "firstName"
+    val requiredKey = "guardianName.error.firstName.required"
+    val lengthKey = "guardianName.error.firstName.length"
+    val maxLength = 100
+
+    behave like fieldThatBindsValidData(
+      form,
+      fieldName,
+      stringsWithMaxLength(maxLength)
+    )
+
+    behave like fieldWithMaxLength(
+      form,
+      fieldName,
+      maxLength = maxLength,
+      lengthError = FormError(fieldName, lengthKey, Seq(maxLength, "first"))
+    )
+
+    behave like mandatoryField(
+      form,
+      fieldName,
+      requiredError = FormError(fieldName, requiredKey, Seq("first"))
+    )
+  }
+
+  ".middleNames" - {
+
+    val fieldName = "middleNames"
+    val lengthKey = "guardianName.error.middleNames.length"
+    val maxLength = 100
+
+    behave like fieldThatBindsValidData(
+      form,
+      fieldName,
+      stringsWithMaxLength(maxLength)
+    )
+
+    behave like fieldWithMaxLength(
+      form,
+      fieldName,
+      maxLength = maxLength,
+      lengthError = FormError(fieldName, lengthKey, Seq(maxLength, "first"))
+    )
+
+  }
+
+  ".lastName" - {
+
+    val fieldName = "lastName"
+    val requiredKey = "guardianName.error.lastName.required"
+    val lengthKey = "guardianName.error.lastName.length"
+    val maxLength = 100
+
+    behave like fieldThatBindsValidData(
+      form,
+      fieldName,
+      stringsWithMaxLength(maxLength)
+    )
+
+    behave like fieldWithMaxLength(
+      form,
+      fieldName,
+      maxLength = maxLength,
+      lengthError = FormError(fieldName, lengthKey, Seq(maxLength, "first"))
+    )
+
+    behave like mandatoryField(
+      form,
+      fieldName,
+      requiredError = FormError(fieldName, requiredKey, Seq("first"))
+    )
+  }
+}
