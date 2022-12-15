28de8ee2e93f59e3268019b67d5fe931f6a5fc69
Nick Wilson <7313396+TheNickWilson@users.noreply.github.com>
2022-12-15 13:36:59 +0000

    Add Check Partner Details page

---
+++ app/controllers/partner/CheckPartnerDetailsController.scala
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.partner
+
+import controllers.actions.{DataRequiredAction, DataRetrievalAction, IdentifierAction}
+import pages.EmptyWaypoints
+import pages.partner.CheckPartnerDetailsPage
+import play.api.i18n.{I18nSupport, MessagesApi}
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
+import views.html.partner.CheckPartnerDetailsView
+import viewmodels.checkAnswers.partner._
+import viewmodels.govuk.summarylist._
+
+import javax.inject.Inject
+
+class CheckPartnerDetailsController  @Inject()(
+                                                  override val messagesApi: MessagesApi,
+                                                  identify: IdentifierAction,
+                                                  getData: DataRetrievalAction,
+                                                  requireData: DataRequiredAction,
+                                                  val controllerComponents: MessagesControllerComponents,
+                                                  view: CheckPartnerDetailsView
+                                                ) extends FrontendBaseController with I18nSupport {
+
+  def onPageLoad: Action[AnyContent] = (identify andThen getData andThen requireData) {
+    implicit request =>
+
+      val thisPage = CheckPartnerDetailsPage
+      val waypoints = EmptyWaypoints
+
+      val partnerDetails = SummaryListViewModel(
+        rows = Seq(
+          PartnerNameSummary.row(request.userAnswers, waypoints, thisPage),
+          PartnerNinoKnownSummary.row(request.userAnswers, waypoints, thisPage),
+          PartnerNinoSummary.row(request.userAnswers, waypoints, thisPage),
+          PartnerDateOfBirthSummary.row(request.userAnswers, waypoints, thisPage),
+          PartnerNationalitySummary.row(request.userAnswers, waypoints, thisPage),
+          PartnerClaimingChildBenefitSummary.row(request.userAnswers, waypoints, thisPage),
+          PartnerEldestChildNameSummary.row(request.userAnswers, waypoints, thisPage),
+          PartnerEldestChildDateOfBirthSummary.row(request.userAnswers, waypoints, thisPage)
+        ).flatten
+      )
+      Ok(view(partnerDetails))
+  }
+
+  def onSubmit: Action[AnyContent] = (identify andThen getData andThen requireData) {
+    implicit request =>
+      Redirect(CheckPartnerDetailsPage.navigate(EmptyWaypoints, request.userAnswers, request.userAnswers).route)
+  }
+}
---
+++ test/controllers/partner/CheckPartnerDetailsControllerSpec.scala
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.partner
+
+import base.SpecBase
+import controllers.partner.routes
+import pages.EmptyWaypoints
+import pages.partner.CheckPartnerDetailsPage
+import play.api.test.FakeRequest
+import play.api.test.Helpers._
+import viewmodels.govuk.SummaryListFluency
+import views.html.partner.CheckPartnerDetailsView
+
+class CheckPartnerDetailsControllerSpec extends SpecBase with SummaryListFluency {
+
+  "Check Partner Details" - {
+
+    "must return OK and the correct view for a GET" in {
+
+      val app = applicationBuilder(Some(emptyUserAnswers)).build()
+
+      running(app) {
+        val request = FakeRequest(GET, routes.CheckPartnerDetailsController.onPageLoad.url)
+
+        val result = route(app, request).value
+
+        val view = app.injector.instanceOf[CheckPartnerDetailsView]
+        val emptyList = SummaryListViewModel(Nil)
+
+        status(result) mustEqual OK
+        contentAsString(result) mustEqual view(emptyList)(request, messages(app)).toString
+      }
+    }
+
+    "must redirect to the next page for a POST" in {
+
+      val app = applicationBuilder(Some(emptyUserAnswers)).build()
+
+      running(app) {
+        val request = FakeRequest(POST, routes.CheckPartnerDetailsController.onSubmit.url)
+
+        val result = route(app, request).value
+
+        status(result) mustEqual SEE_OTHER
+        redirectLocation(result).value mustEqual CheckPartnerDetailsPage.navigate(EmptyWaypoints, emptyUserAnswers, emptyUserAnswers).route.url
+      }
+    }
+  }
+}
