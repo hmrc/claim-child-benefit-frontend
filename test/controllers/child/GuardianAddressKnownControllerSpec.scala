22444b781b9caac3e4d0d88972bdb54dc96fe4db
Nick Wilson <7313396+TheNickWilson@users.noreply.github.com>
2022-12-21 12:44:37 +0000

    Add Guardian Name and Address Known pages

---
+++ test/controllers/child/GuardianAddressKnownControllerSpec.scala
@@ -0,0 +1,160 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.child
+
+import base.SpecBase
+import controllers.{routes => baseRoutes}
+import forms.child.GuardianAddressKnownFormProvider
+import models.AdultName
+import org.mockito.ArgumentMatchers.{any, eq => eqTo}
+import org.mockito.Mockito.{times, verify, when}
+import org.scalatestplus.mockito.MockitoSugar
+import pages.child.{GuardianAddressKnownPage, GuardianNamePage}
+import pages.{EmptyWaypoints, child}
+import play.api.inject.bind
+import play.api.test.FakeRequest
+import play.api.test.Helpers._
+import services.UserDataService
+import views.html.child.GuardianAddressKnownView
+
+import scala.concurrent.Future
+
+class GuardianAddressKnownControllerSpec extends SpecBase with MockitoSugar {
+
+  private val name = AdultName(None, "first", None, "last")
+  private val baseAnswers = emptyUserAnswers.set(GuardianNamePage(index), name).success.value
+
+  val formProvider = new GuardianAddressKnownFormProvider()
+  val form = formProvider(name)
+  private val waypoints = EmptyWaypoints
+
+  lazy val guardianAddressKnownRoute = routes.GuardianAddressKnownController.onPageLoad(waypoints, index).url
+
+  "GuardianAddressKnown Controller" - {
+
+    "must return OK and the correct view for a GET" in {
+
+      val application = applicationBuilder(userAnswers = Some(baseAnswers)).build()
+
+      running(application) {
+        val request = FakeRequest(GET, guardianAddressKnownRoute)
+
+        val result = route(application, request).value
+
+        val view = application.injector.instanceOf[GuardianAddressKnownView]
+
+        status(result) mustEqual OK
+        contentAsString(result) mustEqual view(form, waypoints, index, name)(request, messages(application)).toString
+      }
+    }
+
+    "must populate the view correctly on a GET when the question has previously been answered" in {
+
+      val userAnswers = baseAnswers.set(GuardianAddressKnownPage(index), true).success.value
+
+      val application = applicationBuilder(userAnswers = Some(userAnswers)).build()
+
+      running(application) {
+        val request = FakeRequest(GET, guardianAddressKnownRoute)
+
+        val view = application.injector.instanceOf[GuardianAddressKnownView]
+
+        val result = route(application, request).value
+
+        status(result) mustEqual OK
+        contentAsString(result) mustEqual view(form.fill(true), waypoints, index, name)(request, messages(application)).toString
+      }
+    }
+
+    "must save the answer and redirect to the next page when valid data is submitted" in {
+
+      val mockUserDataService = mock[UserDataService]
+
+      when(mockUserDataService.set(any())) thenReturn Future.successful(true)
+
+      val application =
+        applicationBuilder(userAnswers = Some(baseAnswers))
+          .overrides(
+            bind[UserDataService].toInstance(mockUserDataService)
+          )
+          .build()
+
+      running(application) {
+        val request =
+          FakeRequest(POST, guardianAddressKnownRoute)
+            .withFormUrlEncodedBody(("value", "true"))
+
+        val result = route(application, request).value
+        val expectedAnswers = baseAnswers.set(child.GuardianAddressKnownPage(index), true).success.value
+
+        status(result) mustEqual SEE_OTHER
+        redirectLocation(result).value mustEqual child.GuardianAddressKnownPage(index).navigate(waypoints, emptyUserAnswers, expectedAnswers).url
+        verify(mockUserDataService, times(1)).set(eqTo(expectedAnswers))
+      }
+    }
+
+    "must return a Bad Request and errors when invalid data is submitted" in {
+
+      val application = applicationBuilder(userAnswers = Some(baseAnswers)).build()
+
+      running(application) {
+        val request =
+          FakeRequest(POST, guardianAddressKnownRoute)
+            .withFormUrlEncodedBody(("value", ""))
+
+        val boundForm = form.bind(Map("value" -> ""))
+
+        val view = application.injector.instanceOf[GuardianAddressKnownView]
+
+        val result = route(application, request).value
+
+        status(result) mustEqual BAD_REQUEST
+        contentAsString(result) mustEqual view(boundForm, waypoints, index, name)(request, messages(application)).toString
+      }
+    }
+
+    "must redirect to Journey Recovery for a GET if no existing data is found" in {
+
+      val application = applicationBuilder(userAnswers = None).build()
+
+      running(application) {
+        val request = FakeRequest(GET, guardianAddressKnownRoute)
+
+        val result = route(application, request).value
+
+        status(result) mustEqual SEE_OTHER
+        redirectLocation(result).value mustEqual baseRoutes.JourneyRecoveryController.onPageLoad().url
+      }
+    }
+
+    "must redirect to Journey Recovery for a POST if no existing data is found" in {
+
+      val application = applicationBuilder(userAnswers = None).build()
+
+      running(application) {
+        val request =
+          FakeRequest(POST, guardianAddressKnownRoute)
+            .withFormUrlEncodedBody(("value", "true"))
+
+        val result = route(application, request).value
+
+        status(result) mustEqual SEE_OTHER
+        redirectLocation(result).value mustEqual baseRoutes.JourneyRecoveryController.onPageLoad().url
+      }
+    }
+  }
+}
