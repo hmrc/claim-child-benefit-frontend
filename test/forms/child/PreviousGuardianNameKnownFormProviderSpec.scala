af909185b1ecf58902f6faf8f963df637acd9462
Nick Wilson <7313396+TheNickWilson@users.noreply.github.com>
2022-12-21 11:43:30 +0000

    Add Previous Guardian Name and Address Known pages

---
+++ test/forms/child/PreviousGuardianNameKnownFormProviderSpec.scala
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package forms.child
+
+import forms.behaviours.BooleanFieldBehaviours
+import models.AdultName
+import play.api.data.FormError
+
+class PreviousGuardianNameKnownFormProviderSpec extends BooleanFieldBehaviours {
+
+  val requiredKey = "previousGuardianNameKnown.error.required"
+  val invalidKey = "error.boolean"
+
+  val form = new PreviousGuardianNameKnownFormProvider()()
+
+  ".value" - {
+
+    val fieldName = "value"
+
+    behave like booleanField(
+      form,
+      fieldName,
+      invalidError = FormError(fieldName, invalidKey)
+    )
+
+    behave like mandatoryField(
+      form,
+      fieldName,
+      requiredError = FormError(fieldName, requiredKey)
+    )
+  }
+}
