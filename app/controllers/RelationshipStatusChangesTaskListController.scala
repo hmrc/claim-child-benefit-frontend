d3e46a93ac66f4b4aa01c4401cb9164aeeee04ba
Nick Wilson <7313396+TheNickWilson@users.noreply.github.com>
2023-01-13 14:49:57 +0000

    Implement `task list changed` pages

--- app/controllers/RelationshipStatusChangesTaskListController.scala
+++ app/controllers/RelationshipStatusChangesTaskListController.scala
@@ -17,12 +17,13 @@
 package controllers
 
 import controllers.actions._
+import models.TaskListSectionChange._
 import pages.Waypoints
 import pages.RelationshipStatusChangesTaskListPage
 import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
-import views.html.RelationshipStatusChangesTaskListView
+import views.html._
 
 import javax.inject.Inject
 
@@ -32,12 +33,36 @@ class RelationshipStatusChangesTaskListController @Inject()(
                                               getData: DataRetrievalAction,
                                               requireData: DataRequiredAction,
                                               val controllerComponents: MessagesControllerComponents,
-                                              view: RelationshipStatusChangesTaskListView
-                                            ) extends FrontendBaseController with I18nSupport {
+                                              paymentRemovedView: RelationshipStatusChangesTaskListPaymentRemovedView,
+                                              partnerRemovedView: RelationshipStatusChangesTaskListPartnerRemovedView,
+                                              partnerRequiredView: RelationshipStatusChangesTaskListPartnerRequiredView,
+                                              paymentRemovedPartnerRemovedView: RelationshipStatusChangesTaskListPaymentRemovedPartnerRemovedView,
+                                              paymentRemovedPartnerRequiredView: RelationshipStatusChangesTaskListPaymentRemovedPartnerRequiredView
+                                            )
+  extends FrontendBaseController
+    with I18nSupport
+    with AnswerExtractor {
 
   def onPageLoad(waypoints: Waypoints): Action[AnyContent] = (identify andThen getData andThen requireData) {
     implicit request =>
-      Ok(view(waypoints))
+      getAnswer(RelationshipStatusChangesTaskListPage) {
+        sections =>
+
+          val paymentRemoved = Set(PaymentDetailsRemoved)
+          val partnerRemoved = Set(PartnerDetailsRemoved)
+          val partnerRequired = Set(PartnerDetailsRequired)
+          val paymentRemovedPartnerRemoved = Set(PaymentDetailsRemoved, PartnerDetailsRemoved)
+          val paymentRemovedPartnerRequired = Set(PaymentDetailsRemoved, PartnerDetailsRequired)
+
+          sections match {
+            case x if x == paymentRemoved => Ok(paymentRemovedView(waypoints))
+            case x if x == partnerRemoved => Ok(partnerRemovedView(waypoints))
+            case x if x == partnerRequired => Ok(partnerRequiredView(waypoints))
+            case x if x == paymentRemovedPartnerRemoved => Ok(paymentRemovedPartnerRemovedView(waypoints))
+            case x if x == paymentRemovedPartnerRequired => Ok(paymentRemovedPartnerRequiredView(waypoints))
+            case _ => Redirect(routes.JourneyRecoveryController.onPageLoad())
+          }
+      }
   }
 
   def onSubmit(waypoints: Waypoints): Action[AnyContent] = (identify andThen getData andThen requireData) {
--- test/controllers/RelationshipStatusChangesTaskListControllerSpec.scala
+++ test/controllers/RelationshipStatusChangesTaskListControllerSpec.scala
@@ -17,10 +17,12 @@
 package controllers
 
 import base.SpecBase
+import models.TaskListSectionChange
+import models.TaskListSectionChange.{PartnerDetailsRemoved, PartnerDetailsRequired, PaymentDetailsRemoved}
 import pages.{EmptyWaypoints, RelationshipStatusChangesTaskListPage}
 import play.api.test.FakeRequest
 import play.api.test.Helpers._
-import views.html.RelationshipStatusChangesTaskListView
+import views.html._
 
 class RelationshipStatusChangesTaskListControllerSpec extends SpecBase {
 
@@ -28,19 +30,91 @@ class RelationshipStatusChangesTaskListControllerSpec extends SpecBase {
 
   "RelationshipStatusChangesTaskList Controller" - {
 
-    "must return OK and the correct view for a GET" in {
+    "must return OK and the correct view for a GET" - {
 
-      val application = applicationBuilder(userAnswers = Some(emptyUserAnswers)).build()
+      "when the user's payment details have been removed" in {
 
-      running(application) {
-        val request = FakeRequest(GET, routes.RelationshipStatusChangesTaskListController.onPageLoad().url)
+        val answers = emptyUserAnswers.set(RelationshipStatusChangesTaskListPage, Set[TaskListSectionChange](PaymentDetailsRemoved)).success.value
+        val application = applicationBuilder(userAnswers = Some(answers)).build()
 
-        val result = route(application, request).value
+        running(application) {
+          val request = FakeRequest(GET, routes.RelationshipStatusChangesTaskListController.onPageLoad().url)
 
-        val view = application.injector.instanceOf[RelationshipStatusChangesTaskListView]
+          val result = route(application, request).value
 
-        status(result) mustEqual OK
-        contentAsString(result) mustEqual view(waypoints)(request, messages(application)).toString
+          val view = application.injector.instanceOf[RelationshipStatusChangesTaskListPaymentRemovedView]
+
+          status(result) mustEqual OK
+          contentAsString(result) mustEqual view(waypoints)(request, messages(application)).toString
+        }
+      }
+
+      "when the user's partner details have been removed" in {
+
+        val answers = emptyUserAnswers.set(RelationshipStatusChangesTaskListPage, Set[TaskListSectionChange](PartnerDetailsRemoved)).success.value
+        val application = applicationBuilder(userAnswers = Some(answers)).build()
+
+        running(application) {
+          val request = FakeRequest(GET, routes.RelationshipStatusChangesTaskListController.onPageLoad().url)
+
+          val result = route(application, request).value
+
+          val view = application.injector.instanceOf[RelationshipStatusChangesTaskListPartnerRemovedView]
+
+          status(result) mustEqual OK
+          contentAsString(result) mustEqual view(waypoints)(request, messages(application)).toString
+        }
+      }
+
+      "when the user's partner details are required" in {
+
+        val answers = emptyUserAnswers.set(RelationshipStatusChangesTaskListPage, Set[TaskListSectionChange](PartnerDetailsRequired)).success.value
+        val application = applicationBuilder(userAnswers = Some(answers)).build()
+
+        running(application) {
+          val request = FakeRequest(GET, routes.RelationshipStatusChangesTaskListController.onPageLoad().url)
+
+          val result = route(application, request).value
+
+          val view = application.injector.instanceOf[RelationshipStatusChangesTaskListPartnerRequiredView]
+
+          status(result) mustEqual OK
+          contentAsString(result) mustEqual view(waypoints)(request, messages(application)).toString
+        }
+      }
+
+      "when the user's payment and partner details have been removed" in {
+
+        val answers = emptyUserAnswers.set(RelationshipStatusChangesTaskListPage, Set[TaskListSectionChange](PaymentDetailsRemoved, PartnerDetailsRemoved)).success.value
+        val application = applicationBuilder(userAnswers = Some(answers)).build()
+
+        running(application) {
+          val request = FakeRequest(GET, routes.RelationshipStatusChangesTaskListController.onPageLoad().url)
+
+          val result = route(application, request).value
+
+          val view = application.injector.instanceOf[RelationshipStatusChangesTaskListPaymentRemovedPartnerRemovedView]
+
+          status(result) mustEqual OK
+          contentAsString(result) mustEqual view(waypoints)(request, messages(application)).toString
+        }
+      }
+
+      "when the user's payment details have been removed and partner details are required" in {
+
+        val answers = emptyUserAnswers.set(RelationshipStatusChangesTaskListPage, Set[TaskListSectionChange](PaymentDetailsRemoved, PartnerDetailsRequired)).success.value
+        val application = applicationBuilder(userAnswers = Some(answers)).build()
+
+        running(application) {
+          val request = FakeRequest(GET, routes.RelationshipStatusChangesTaskListController.onPageLoad().url)
+
+          val result = route(application, request).value
+
+          val view = application.injector.instanceOf[RelationshipStatusChangesTaskListPaymentRemovedPartnerRequiredView]
+
+          status(result) mustEqual OK
+          contentAsString(result) mustEqual view(waypoints)(request, messages(application)).toString
+        }
       }
     }
 
