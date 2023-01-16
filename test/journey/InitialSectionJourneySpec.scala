c685e3584b8911874c48285c9b8355b7320eff38
Nick Wilson <7313396+TheNickWilson@users.noreply.github.com>
2023-01-16 19:49:17 +0000

    Change initial section to consist of kickout only

---
+++ test/journey/InitialSectionJourneySpec.scala
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package journey
+
+import generators.ModelGenerators
+import org.scalatest.freespec.AnyFreeSpec
+import pages._
+
+class InitialSectionJourneySpec extends AnyFreeSpec with JourneyHelpers with ModelGenerators {
+
+  "users who have recently claimed must go to the Already Claimed page" in {
+
+    startingFrom(RecentlyClaimedPage)
+      .run(
+        submitAnswer(RecentlyClaimedPage, true),
+        pageMustBe(AlreadyClaimedPage)
+      )
+  }
+
+  "users who have not recently claimed must go to the task list" in {
+
+    startingFrom(RecentlyClaimedPage)
+      .run(
+        submitAnswer(RecentlyClaimedPage, false),
+        pageMustBe(TaskListPage)
+      )
+  }
+}
