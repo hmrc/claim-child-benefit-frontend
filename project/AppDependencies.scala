c2bacc990297443e8a112e265a26a0f23e5003e1
Michael Wolfendale <4563722+wolfendale@users.noreply.github.com>
2023-02-09 12:47:45 +0000

    Update test dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,13 +1,14 @@
 import sbt._
 
 object AppDependencies {
-  import play.core.PlayVersion
+
+  private val bootstrapVersion = "7.6.0"
 
   val compile = Seq(
     play.sbt.PlayImport.ws,
     "uk.gov.hmrc"                   %% "play-frontend-hmrc"             % "3.23.0-play-28",
     "uk.gov.hmrc"                   %% "play-conditional-form-mapping"  % "1.11.0-play-28",
-    "uk.gov.hmrc"                   %% "bootstrap-frontend-play-28"     % "7.6.0",
+    "uk.gov.hmrc"                   %% "bootstrap-frontend-play-28"     % bootstrapVersion,
     "uk.gov.hmrc.mongo"             %% "hmrc-mongo-play-28"             % "0.71.0",
     "org.typelevel"                 %% "cats-core"                      % "2.3.0",
     "uk.gov.hmrc"                   %% "domain"                         % "8.1.0-play-28",
@@ -17,13 +18,12 @@ object AppDependencies {
   )
 
   val test = Seq(
+    "uk.gov.hmrc"             %% "bootstrap-test-play-28"  % bootstrapVersion,
     "org.scalatest"           %% "scalatest"               % "3.2.10",
     "org.scalatestplus"       %% "scalacheck-1-15"         % "3.2.10.0",
     "org.scalatestplus"       %% "mockito-3-4"             % "3.2.10.0",
-    "org.scalatestplus.play"  %% "scalatestplus-play"      % "5.1.0",
     "org.pegdown"             %  "pegdown"                 % "1.6.0",
     "org.jsoup"               %  "jsoup"                   % "1.14.3",
-    "com.typesafe.play"       %% "play-test"               % PlayVersion.current,
     "org.mockito"             %% "mockito-scala"           % "1.16.42",
     "org.scalacheck"          %% "scalacheck"              % "1.15.4",
     "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-28" % "0.71.0",
