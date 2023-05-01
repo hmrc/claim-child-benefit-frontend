713d3e84c61b56ca24702b76a21537e59d34a724
Nick Wilson <7313396+TheNickWilson@users.noreply.github.com>
2023-05-01 17:51:03 +0100

    Add action to check for recent claims

---
+++ app/controllers/actions/CheckRecentClaimsAction.scala
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.actions
+
+import connectors.ClaimChildBenefitConnector
+import controllers.routes
+import models.requests.{AuthenticatedIdentifierRequest, IdentifierRequest, UnauthenticatedIdentifierRequest}
+import play.api.mvc.Results.Redirect
+import play.api.mvc.{ActionFilter, Result}
+import uk.gov.hmrc.play.http.HeaderCarrierConverter
+
+import javax.inject.Inject
+import scala.concurrent.{ExecutionContext, Future}
+
+class CheckRecentClaimsAction @Inject() (connector: ClaimChildBenefitConnector)
+                                        (implicit val executionContext: ExecutionContext) extends ActionFilter[IdentifierRequest] {
+
+  override protected def filter[A](request: IdentifierRequest[A]): Future[Option[Result]] =
+    request match {
+      case _: AuthenticatedIdentifierRequest[_] =>
+        val hc = HeaderCarrierConverter.fromRequestAndSession(request, request.session)
+
+        connector
+          .getRecentClaim()(hc)
+          .map(_.map(_ => Redirect(routes.RecentlySubmittedController.onPageLoad())))
+
+      case _: UnauthenticatedIdentifierRequest[_] =>
+        Future.successful(None)
+    }
+}
