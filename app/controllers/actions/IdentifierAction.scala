fa4c254e75a9b925192bc0a5307cadb7c2ed2055
Nick Wilson <7313396+TheNickWilson@users.noreply.github.com>
2023-06-08 17:51:49 +0100

    Remove session idenfitier action

--- app/controllers/actions/IdentifierAction.scala
+++ app/controllers/actions/IdentifierAction.scala
@@ -16,32 +16,90 @@
 
 package controllers.actions
 
-import com.google.inject.Inject
+import config.FrontendAppConfig
+import controllers.auth.{routes => authRoutes}
 import controllers.routes
-import models.requests.{IdentifierRequest, UnauthenticatedIdentifierRequest}
-import play.api.mvc.Results._
-import play.api.mvc._
+import models.requests.{AuthenticatedIdentifierRequest, IdentifierRequest, UnauthenticatedIdentifierRequest}
+import play.api.mvc.Results.Redirect
+import play.api.mvc.{ActionBuilder, ActionFunction, AnyContent, BodyParsers, Call, Request, Result}
+import uk.gov.hmrc.auth.core.AffinityGroup.{Agent, Individual, Organisation}
+import uk.gov.hmrc.auth.core.retrieve.v2.Retrievals
+import uk.gov.hmrc.auth.core.retrieve.~
+import uk.gov.hmrc.auth.core._
 import uk.gov.hmrc.http.HeaderCarrier
 import uk.gov.hmrc.play.http.HeaderCarrierConverter
 
+import javax.inject.Inject
+import scala.annotation.nowarn
 import scala.concurrent.{ExecutionContext, Future}
 
 trait IdentifierAction extends ActionBuilder[IdentifierRequest, AnyContent] with ActionFunction[Request, IdentifierRequest]
 
-class SessionIdentifierAction @Inject()(
-                                         val parser: BodyParsers.Default
-                                       )
-                                       (implicit val executionContext: ExecutionContext) extends IdentifierAction {
+class OptionalAuthIdentifierAction @Inject()(
+                                              val authConnector: AuthConnector,
+                                              val parser: BodyParsers.Default,
+                                              config: FrontendAppConfig
+                                            )(implicit val executionContext: ExecutionContext) extends IdentifierAction with AuthorisedFunctions {
 
+  @nowarn()
   override def invokeBlock[A](request: Request[A], block: IdentifierRequest[A] => Future[Result]): Future[Result] = {
 
     implicit val hc: HeaderCarrier = HeaderCarrierConverter.fromRequestAndSession(request, request.session)
 
-    hc.sessionId match {
-      case Some(session) =>
-        block(UnauthenticatedIdentifierRequest(request, session.value))
-      case None =>
-        Future.successful(Redirect(routes.JourneyRecoveryController.onPageLoad()))
+    authorised()
+      .retrieve(
+        Retrievals.affinityGroup and
+          Retrievals.credentialStrength and
+          Retrievals.confidenceLevel and
+          Retrievals.internalId and
+          Retrievals.nino
+      ) {
+        case Some(Agent) ~ _ ~ _ ~ _ ~ _ =>
+          redirectTo(authRoutes.AuthController.unsupportedAffinityGroupAgent(config.loginContinueUrl + request.path))
+
+        case Some(Organisation) ~ _ ~ _ ~ _ ~ _ =>
+          redirectTo(authRoutes.AuthController.unsupportedAffinityGroupOrganisation(config.loginContinueUrl + request.path))
+
+        case Some(Individual) ~ Some(CredentialStrength.weak) ~ _ ~ _ ~ _ =>
+          upliftMfa(request)
+
+        case Some(Individual) ~ _ ~ confidenceLevel ~ _ ~ _ if confidenceLevel < ConfidenceLevel.L250  =>
+          upliftIv(request)
+
+        case Some(Individual) ~ Some(CredentialStrength.strong) ~ _ ~ Some(internalId) ~ Some(nino) =>
+          block(AuthenticatedIdentifierRequest(request, internalId, nino))
+    }.recoverWith {
+      case _: NoActiveSession =>
+        hc.sessionId match {
+          case Some(sessionId) =>
+            block(UnauthenticatedIdentifierRequest(request, sessionId.value))
+          case None =>
+            Future.successful(Redirect(routes.JourneyRecoveryController.onPageLoad()))
+        }
     }
   }
+
+  private def redirectTo(call: Call): Future[Result] =
+    Future.successful(Redirect(call))
+
+  private def upliftMfa(request: Request[_]): Future[Result] = {
+    Future.successful(Redirect(
+      config.upliftMfaUrl,
+      Map(
+        "origin" -> Seq(config.origin),
+        "continueUrl" -> Seq(config.loginContinueUrl + request.path)
+      )
+    ))
+  }
+
+  private def upliftIv(request: Request[_]): Future[Result] =
+    Future.successful(Redirect(
+      config.upliftIvUrl,
+      Map(
+        "origin" -> Seq(config.origin),
+        "confidenceLevel" -> Seq(ConfidenceLevel.L250.toString),
+        "completionURL" -> Seq(config.loginContinueUrl + request.path),
+        "failureURL" -> Seq(config.loginContinueUrl + authRoutes.IvController.handleIvFailure(request.path, None).url)
+      )
+    ))
 }
